4a9dc9019abeed7a9eca49459b9738afe73cefe6
Oliver Pham <Ngocthaipham1996@gmail.com>
2019-06-16 22:23:03 +0700

    GFDS

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     parameters {
-        string(name: 'Greeting', defaultValue: 'Hello', description: 'How should I greet the world?')
+        string(name: 'Greeting', defaultValue: 'HELLO', description: 'How should I greet the world?')
     }
     stages {
         stage('Example') {
