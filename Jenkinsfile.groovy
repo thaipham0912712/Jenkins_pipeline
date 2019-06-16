abe925b463f029d3398da81f14eb5f0836a8792c
Oliver Pham <Ngocthaipham1996@gmail.com>
2019-06-16 15:14:20 +0700

    Add the first pipeline

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build'){
+            step {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            step {
+                echo 'Testing..'
+            }
+        }
+        stage ('Deploy') {
+            step {
+                echo "Deploying..."
+            }
+        }
+    }
+}
