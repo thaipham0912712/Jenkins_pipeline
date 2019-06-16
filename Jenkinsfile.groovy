a922087c16237a3d4ea22d3cc63e3c010387c61d
Oliver Pham <Ngocthaipham1996@gmail.com>
2019-06-16 17:07:24 +0700

    Build stage

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,13 @@
-node {
-    stage('Build') {
-        echo 'Building....'
-    }
-    stage('Test') {
-        echo 'Testing....'
-    }
-    stage('Deploy') {
-        echo 'Deploying....'
+//Jenkinsfile (Declarative Pipeline)
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'make' 
+                archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true 
+            }
+        }
     }
 }
