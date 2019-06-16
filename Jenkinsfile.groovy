d3656345b1050080b4c7ac430db605a506c7fbf6
Oliver Pham <ngocthaipham1996@gmail.com>
2019-06-16 17:12:45 +0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,6 @@
-//Jenkinsfile (Declarative Pipeline)
-pipeline {
-    agent any
-
-    stages {
-        stage('Build') {
-            steps {
-                sh 'make' 
-                archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true 
-            }
-        }
+node {
+    stage('Build') {
+        sh 'make' 
+        archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true 
     }
 }
