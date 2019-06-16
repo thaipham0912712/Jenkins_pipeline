3fa1e109adb7ae8fe382a89fbead3c8d37c6f436
Oliver Pham <Ngocthaipham1996@gmail.com>
2019-06-16 15:57:39 +0700

    simplize Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,11 @@
-pipeline {
-    agent any
-
-    stages {
-        stage('Build') {
-            steps {
-                echo 'Building..'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
+node {
+    stage('Build') {
+        echo 'Building....'
+    }
+    stage('Test') {
+        echo 'Testing....'
+    }
+    stage('Deploy') {
+        echo 'Deploying....'
     }
 }
