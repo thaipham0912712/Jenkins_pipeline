2a232dc8ed513cf80ce282ff39bfdc6f40749207
Oliver Pham <Ngocthaipham1996@gmail.com>
2019-06-16 17:30:43 +0700

    nothing

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,9 @@
 pipeline {
     agent any
-
     stages {
-        stage('Deploy') {
+        stage('Example') {
             steps {
-                def username = 'Jenkins'
-                echo 'Hello Mr. ${username}'
-                echo "I said, Hello Mr. ${username}"
+                echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
             }
         }
     }
